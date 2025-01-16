90e713c7e11168913893212a777ceb318fa423e6
TanithFlory <tanithflory7770@gmail.com>
2025-01-16 16:58:57 +0530

    update: css

--- src/app/components/pic-of-the-day/pic-of-the-day.component.ts
+++ src/app/components/pic-of-the-day/pic-of-the-day.component.ts
@@ -4,10 +4,11 @@ import { PicOfTheDayService } from '../../services/pic-of-the-day.service';
 import { catchError } from 'rxjs';
 import { PicOfTheDay } from '../../../types';
 import { SafeUrlPipe } from '../../pipes/safe-url.pipe';
+import { TitleComponent } from '../utils/title/title.component';
 
 @Component({
   selector: 'app-pic-of-the-day',
-  imports: [SectionWrapperDirective, SafeUrlPipe],
+  imports: [SectionWrapperDirective, SafeUrlPipe, TitleComponent],
   templateUrl: './pic-of-the-day.component.html',
   styleUrl: './pic-of-the-day.component.css',
 })
